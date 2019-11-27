a2b4b78de3d25e3739cf51355f61213e9ba247cc
hoangvu.nguyen <hoangvu.nguyen@evizi.com>
2019-11-27 10:48:27 +0700

    Update project to suitable with 0.61.4

--- src/helper/utilities.js
+++
@@ -1,5 +0,0 @@
-import { Platform, Dimensions } from 'react-native';
-
-const { height, width } = Dimensions.get('window');
-export const isX = (Platform.OS === 'ios' 
-            && (height === 812 || width > 812 || height === 896 || width > 896));
